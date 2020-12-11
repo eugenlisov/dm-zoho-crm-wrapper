5ef304cb053689b9c56bd29ced24176b367ca073
Eugen Lisov <eugen.lisov@gmail.com>
2020-12-11 18:20:27 +0200

    Added comment

--- app/Models/Industries.php
+++ app/Models/Industries.php
@@ -6,7 +6,7 @@ class Industries extends ZohoCRMModules {
 	// Find this here: https://crm.zoho.com/crm/org3130230/settings/api/modules (CRM Settings---->Setup---->Developer space----->APIs)
 	static $module = 'Training_Modules';
 	
-	public static function getIndustriesIdValueList() {
+	public static function getIndustriesIdValueList() { // Used by Zoho Forms
 
 		$industries = static::list();
 
