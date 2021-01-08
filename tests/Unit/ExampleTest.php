8395c13f4be4812be4d81504ef6e19a53f6bc607
Eugen Lisov <eugen.lisov@gmail.com>
2021-01-08 21:05:13 +0200

    Set up PHPUnit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\CustomTestCase;
+
+class ExampleTest extends CustomTestCase
+{
+    public function testBasicTestFeature()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The following snippets uses `PLUGIN` to prefix
+ * the constants and class names. You should replace
+ * it with something that matches your plugin name.
+ */
+// define test environment
+define( 'PLUGIN_PHPUNIT', true );
+
+require_once __DIR__ . '/../vendor/autoload.php';
